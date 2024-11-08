0b8e24d71d64f8231d0cf5eed1ea1ab58b1b50fd
Sanchuan0209 <123540032+testme76@users.noreply.github.com>
2024-11-08 00:58:25 -0800

    navbar mod

--- src/components/Header/NavItem.js
+++ src/components/Header/NavItem.js
@@ -5,8 +5,8 @@ import React from "react";
 const NavItem = ({ navItem = {} }) => {
   const { pathname } = useRouter();
 
-  const { name, href, subNavItems } = navItem;
-  const subHref = subNavItems.map((item) => item.href);
+  const { name, href, subNavItems = [] } = navItem;
+  const subHref = subNavItems?.map((item) => item.href) || [];
   const current = pathname === href || subHref.includes(pathname);
 
   return (
@@ -14,24 +14,28 @@ const NavItem = ({ navItem = {} }) => {
       <Link href={href} legacyBehavior>
         <a href={href}>{name}</a>
       </Link>
-      <ul>
-        {subNavItems.map((subItem) => (
-          <li key={subItem.id}>
-            <Link href={subItem.href} legacyBehavior>
-              <a href={href}>{subItem.name}</a>
-            </Link>
-            <ul>
-              {subItem.subItems?.map((item) => (
-                <li key={item.id}>
-                  <Link href={item.href} legacyBehavior>
-                    <a href={href}>{item.name}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+      {subNavItems?.length > 0 && (
+        <ul>
+          {subNavItems.map((subItem) => (
+            <li key={subItem.id}>
+              <Link href={subItem.href} legacyBehavior>
+                <a href={href}>{subItem.name}</a>
+              </Link>
+              {subItem.subItems?.length > 0 && (
+                <ul>
+                  {subItem.subItems.map((item) => (
+                    <li key={item.id}>
+                      <Link href={item.href} legacyBehavior>
+                        <a href={href}>{item.name}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </li>
   );
 };
--- src/data/NavItems.js
+++ src/data/NavItems.js
@@ -3,57 +3,57 @@ const navItems = [
     id: 1,
     name: "Home",
     href: "/",
-    subNavItems: [
-      {
-        id: 1,
-        name: "Home One",
-        href: "/",
-      },
-      // {
-      //   id: 2,
-      //   name: "Home Two",
-      //   href: "/home2",
-      // },
-      // {
-      //   id: 3,
-      //   name: "Home Three",
-      //   href: "/home3",
-      // },
-      // {
-      //   id: 4,
-      //   name: "Header Styles",
-      //   href: "/",
-      //   subItems: [
-      //     {
-      //       id: 1,
-      //       name: "Header One",
-      //       href: "",
-      //     },
-      //     { id: 2, name: "Header Two", href: "" },
-      //     { id: 3, name: "Header Three", href: "" },
-      //   ],
-      // },
-    ],
+    // subNavItems: [
+    //   {
+    //     id: 1,
+    //     name: "Home One",
+    //     href: "/",
+    //   },
+    //   // {
+    //   //   id: 2,
+    //   //   name: "Home Two",
+    //   //   href: "/home2",
+    //   // },
+    //   // {
+    //   //   id: 3,
+    //   //   name: "Home Three",
+    //   //   href: "/home3",
+    //   // },
+    //   // {
+    //   //   id: 4,
+    //   //   name: "Header Styles",
+    //   //   href: "/",
+    //   //   subItems: [
+    //   //     {
+    //   //       id: 1,
+    //   //       name: "Header One",
+    //   //       href: "",
+    //   //     },
+    //   //     { id: 2, name: "Header Two", href: "" },
+    //   //     { id: 3, name: "Header Three", href: "" },
+    //   //   ],
+    //   // },
+    // ],
   },
   {
     id: 2,
     name: "Pages",
     href: "",
-    subNavItems: [
-      { id: 1, name: "About", href: "/about" },
-      //{ id: 2, name: "Volunteers", href: "/volunteers" },
-      //{ id: 3, name: "Gallery", href: "/gallery" },
-      //{ id: 4, name: "Become a Volunteer", href: "/become-volunteer" },
-    ],
+    // subNavItems: [
+    //   { id: 1, name: "About", href: "/about" },
+    //   //{ id: 2, name: "Volunteers", href: "/volunteers" },
+    //   //{ id: 3, name: "Gallery", href: "/gallery" },
+    //   //{ id: 4, name: "Become a Volunteer", href: "/become-volunteer" },
+    // ],
   },
   {
     id: 3,
     name: "Donations",
     href: "/causes",
-    subNavItems: [
-      { id: 1, name: "Causes", href: "/causes" },
-      { id: 2, name: "Causes Details", href: "/causes-details" },
-    ],
+    // subNavItems: [
+    //   { id: 1, name: "Causes", href: "/causes" },
+    //   { id: 2, name: "Causes Details", href: "/causes-details" },
+    // ],
   },
   // {
   //   id: 4,
